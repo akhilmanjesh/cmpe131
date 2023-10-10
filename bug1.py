21962679946e9bdc244266aa7d4ac82382791b6e
Cross A <akhilmanjesh@gmail.com>
2023-10-09 21:31:17 -0700

    Back

--- bug1.py
+++ bug1.py
@@ -1,15 +1,22 @@
+
 class Base:
     def __init__(self, x, y, size):
         self.x = x
         self.y = y
         self.size = size
+        
+    def shape(self):
+        pass
 
 class Circle(Base):
     def __init__(self, x, y, size):
         super().__init__(x, y, size)
 
+    def shape(self):
+        return "This is a circle"
+        
     def draw(self):
-        return f"""
+        return f'''
 ({self.x}, {self.y})
 {self.size}
          , - ~ ~ ~ - ,
@@ -23,4 +30,24 @@ class Circle(Base):
    ,                       ,
      ,                  , '
        ' - , _ _ _ ,  '
-"""
+'''
+
+class Square(Base):
+    def shape(self):
+        return "Square"
+        
+    def draw(self):
+        return f'''
+({self.x}, {self.y}) {self.size}
++{'-' * self.size}+
+|{' ' * self.size}|
++{'-' * self.size}+
+'''
+
+def main():
+    c = Circle(2, 2, 1)
+    print(c.shape())
+    print(c.draw())
+
+if __name__ == "__main__":
+    main()
