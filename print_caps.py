6f0bf9342634339027dcac552a4821db19c1b088
Cross A <akhilmanjesh@gmail.com>
2023-10-10 10:48:23 -0700

    T

--- print_caps.py
+++ print_caps.py
@@ -1,5 +1,11 @@
+# def allcaps(func):
+#     def wrapper():
+#         result = func()
+#         print(result.upper())
+#     return wrapper
+
 def allcaps(func):
     def wrapper():
         result = func()
-        print(result.upper())
+        return result.upper()
     return wrapper
