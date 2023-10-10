f6f035451a247d5dc8c1f2c725ac6a847deeb0ac
Cross A <akhilmanjesh@gmail.com>
2023-10-09 19:57:07 -0700

    T

--- print_caps.py
+++ print_caps.py
@@ -1,7 +1,5 @@
 def allcaps(func):
     def wrapper():
         result = func()
-        if isinstance(result, list):
-            return [item.upper() for item in result]
         return result.upper()
     return wrapper
