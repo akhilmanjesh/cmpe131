e161e49593c09ff7686694e7c5449bfc03c6b465
Cross A <akhilmanjesh@gmail.com>
2023-10-09 19:30:01 -0700

    NC

--- sorted.py
+++ sorted.py
@@ -1,3 +1,4 @@
+
 def sort_dictionary(d):
-    sorted_list = sorted([(name, info[0]) for name, info in d.items()])
+    sorted_list = sorted([(name, info[0]) for name, info in d.items()], reverse=True)
     return sorted_list
